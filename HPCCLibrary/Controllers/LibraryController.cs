cedfce5e05ea4ed53fc6a17ff3e790e42f8422eb
Anuitex <Anuitex@DESKTOP-00VRJM6>
2019-03-12 19:51:17 +0200

    add getallgenre

---
+++ HPCCLibrary/Controllers/LibraryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLogicLayer.Interfaces;
+using EntitiesLayer.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HPCCLibrary.Controllers
+{
+    [Route("api/library")]
+    [ApiController]
+    public class LibraryController : ControllerBase
+    {
+        private ILibraryService libraryService;
+        public LibraryController(ILibraryService libraryService)
+        {
+            this.libraryService = libraryService;
+        }
+
+        [HttpGet]
+        [Route("getallgenre")]
+        public async Task<IEnumerable<Genre>> Get()
+        {
+            return await libraryService.GetAllGenre();
+        }
+    }
+}
